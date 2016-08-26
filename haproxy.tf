13c5db95a6e31e9c294a387d9efcc148fe3c6359
fbarajas <fbarajas@digitalocean.com>
2016-08-26 00:46:49 -0700

    using terraform.tfvars now, dependency issue for ip re-assignment (python-requests) has been corrected. ansible-vault is now being used for API key and heartbeat cluster membership.

--- haproxy.tf
+++ haproxy.tf
@@ -1,40 +1,42 @@
 # haproxy lb and web node set up
 
 resource "digitalocean_droplet" "load_balancer" {
-	count = 2
-	image = "${var.image_slug}"
-	name = "${var.project}-lb-${count.index + 1}"
-	region = "${var.region}"
-	size = "1gb"
-	private_networking = true
-	ssh_keys = ["${var.keys}"]
-	user_data = "${template_file.user_data.rendered}"
-	connection {
-		user = "root"
-		type = "ssh"
-		key_file = "${var.private_key_path}"
-		timeout = "2m"
-	}
+  count              = 2
+  image              = "${var.image_slug}"
+  name               = "${var.project}-lb-${count.index + 1}"
+  region             = "${var.region}"
+  size               = "2gb"
+  private_networking = true
+  ssh_keys           = ["${var.keys}"]
+  user_data          = "${data.template_file.user_data.rendered}"
+
+  connection {
+    user     = "root"
+    type     = "ssh"
+    key_file = "${var.private_key_path}"
+    timeout  = "2m"
+  }
 }
 
 resource "digitalocean_droplet" "web_node" {
-	count = "${var.node_count}"
-	image = "${var.image_slug}"
-	name = "${var.project}-web-${count.index + 1}"
-	region = "${var.region}"
-	size = "512mb"
-	private_networking = true
-	ssh_keys = ["${var.keys}"]
-	user_data = "${template_file.user_data.rendered}"
-	connection {
-		user = "root"
-		type = "ssh"
-		key_file = "${var.private_key_path}"
-		timeout = "2m"
-	}
+  count              = "${var.node_count}"
+  image              = "${var.image_slug}"
+  name               = "${var.project}-web-${count.index + 1}"
+  region             = "${var.region}"
+  size               = "1gb"
+  private_networking = true
+  ssh_keys           = ["${var.keys}"]
+  user_data          = "${data.template_file.user_data.rendered}"
+
+  connection {
+    user     = "root"
+    type     = "ssh"
+    key_file = "${var.private_key_path}"
+    timeout  = "2m"
+  }
 }
 
-resource "template_file" "user_data" {
+data "template_file" "user_data" {
   template = "${file("${path.module}/config/cloud-config.yaml")}"
 
   vars {
@@ -43,6 +45,6 @@ resource "template_file" "user_data" {
 }
 
 resource "digitalocean_floating_ip" "fip" {
-	region = "${var.region}"
-	droplet_id = "${digitalocean_droplet.load_balancer.0.id}"
+  region     = "${var.region}"
+  droplet_id = "${digitalocean_droplet.load_balancer.0.id}"
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,22 @@
 # Set up provider details
+variable "do_token" {}
+
+variable "project" {}
+
+variable "region" {}
+
+variable "image_slug" {}
+
+variable "keys" {}
+
+variable "private_key_path" {}
+
+variable "ssh_fingerprint" {}
+
+variable "public_key" {}
+
+variable "node_count" {}
 
 provider "digitalocean" {
-	token = "${var.do_token}"
+  token = "${var.do_token}"
 }
